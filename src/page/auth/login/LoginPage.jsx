e94ec058e5224a7fb11967e25b749d4b4eb51cfb
Little Boy's h1eudayne <voduchieu42@gmail.com>
2026-01-30 02:42:48 +0700

    Complete fix bug UI responsive Login Page

--- src/page/auth/login/LoginPage.jsx
+++ src/page/auth/login/LoginPage.jsx
@@ -3,13 +3,12 @@ import AuthRight from "../../../components/auth/auth-right/AuthRight";
 
 const LoginPage = () => {
   return (
-    /* SỬA: Đổi d-block thành d-flex cho cả mobile để dùng được align-items-center */
-    <div className="auth-page-wrapper bg-light d-flex align-items-center justify-content-center min-vh-100 py-3 py-lg-4">
-      <div className="container p-0 p-lg-3">
+    <div className="auth-page-wrapper bg-light d-flex align-items-center justify-content-center min-vh-100 py-4">
+      <div className="container p-0">
         <div className="row justify-content-center g-0">
           <div className="col-xl-9 col-lg-10 col-md-11 p-0">
-            {/* Giữ nguyên mx-2 để card không dính sát lề điện thoại */}
-            <div className="auth-card d-flex flex-column flex-lg-row overflow-hidden border-0 shadow-lg rounded-4 mx-2">
+            {/* mx-3 giúp card có khoảng cách với 2 mép điện thoại */}
+            <div className="auth-card d-flex flex-column flex-lg-row overflow-hidden border-0 shadow-lg rounded-4 mx-3 mx-lg-2">
               <AuthLeft />
               <AuthRight />
             </div>
